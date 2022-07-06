b7d8c53f60a750aa058c974cc46357ce7931dfc6
Vitor Hugo <vitork42@gmail.com>
2022-07-06 10:12:33 -0300

    :sparkles: Creation of episodes models; Axios added; Fake API call on the Home page;

---
+++ src/models/Episode.ts
@@ -0,0 +1,11 @@
+export interface Episode {
+  id: string,
+  title: string,
+  members: string,
+  thumbnail: string,
+  publishedAt: string,
+  duration: number,
+  durationAsString: string,
+  description: string,
+  url: string,
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,51 @@
+import { GetStaticProps } from 'next';
+import { format, parseISO} from 'date-fns';
+import { api } from '../services/api';
+import { Episode } from '../models/Episode';
+import enUS from 'date-fns/locale/en-US';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
+interface HomeProps { 
+  episodes: Episode[]
+}
 
-export default function Home() {
+export default function Home(props: HomeProps) {
 
   return (
-    <h1>INDEX</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => { 
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(ep => {
+    return {
+      id: ep.id,
+      title: ep.title,
+      thumbnail: ep.thumbnail,
+      members: ep.members,
+      publishedAt: format(parseISO(ep.published_at), 'MMM d yy', { locale: enUS }),
+      duration: Number(ep.file.duration),
+      durationAsString: convertDurationToTimeString(Number(ep.file.duration)),
+      description: ep.description,
+      url: ep.file.url
+    }
+  })
+
+  return {
+    props: {
+      episodes: episodes,
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
